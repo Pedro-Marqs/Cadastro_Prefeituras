121aa1347c5f8f18e60a45212e3d7fbd9b462911
Pedro <pedrompro01@gmail.com>
2026-03-10 20:37:21 -0300

    Criando projeto

---
+++ src/main/java/com/cadastro_prefeitura/CadastroPrefeituraApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro_prefeitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPrefeituraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroPrefeituraApplication.class, args);
+    }
+
+}
